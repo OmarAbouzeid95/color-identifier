8a33758dbcca9ae2e45382f019de1548fa612423
Omar Abouzeid <omaradham1995@gmail.com>
2026-02-24 15:20:11 -0500

    chore: update global styles to use neobrutalism theme

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		react({
 			babel: {
-				plugins: [['babel-plugin-react-compiler', tailwindcss()]],
+				plugins: [['babel-plugin-react-compiler']],
 			},
 		}),
 	],
