1662e161e7b71a97881d46a34d5a43056d808d85
Omar Abouzeid <omaradham1995@gmail.com>
2026-02-28 19:25:16 -0500

    chore: add max-width to the header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
 	return (
-		<header className='w-full py-2 px-4 border-b-2'>
-			<h1 className='text-xl font-bold'>Color Identifier</h1>
+		<header className='w-full p-4 border-b-2'>
+			<h1 className='text-xl font-bold md:max-w-7xl mx-auto'>
+				Color Identifier
+			</h1>
 		</header>
 	);
 }
