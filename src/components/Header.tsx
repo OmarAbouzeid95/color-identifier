ca05890a841260e76bdfa2dbef242c4437e7adfb
Omar Abouzeid <omaradham1995@gmail.com>
2026-03-11 14:06:51 -0400

    chore: update site header name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
 	return (
 		<header className='w-full p-4 border-b-2'>
 			<h1 className='text-xl font-bold md:max-w-7xl mx-auto'>
-				whatsthatcolor.net
+				whatsthatcolor.site
 			</h1>
 		</header>
 	);
